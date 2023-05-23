b69741c6bf8a549d97664974fcea9477271c2e13
shahulk <shahulhameed0653@gmail.com>
2023-05-23 16:00:10 +0530

    commit

---
+++ bin/fina_const.dart
@@ -0,0 +1,10 @@
+ void main(){
+ String name="manu";
+ int age =25;
+ final  String course;
+ course="flutter";
+ const String batch="python";
+
+ print(course);
+
+ }
---
+++ bin/var_dynamic.dart
@@ -0,0 +1,7 @@
+void main(){
+  var name="shahul";
+  dynamic age=20;
+     age="shahul";
+  print(name);
+  print(age);
+}
